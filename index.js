1307b9d5e4b2cfd8cae2b4c25b0138c2a8103075
Saurav kumar <121569368+sauravkumar0011@users.noreply.github.com>
2025-09-25 21:53:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
     origin:'http://localhost:5173',
+    process.env.FRONTEND_URL,
     credentials:true
 }
 app.use(cors(corsOptions));
